e94b12edf5a219aa95701f2fd54e33b626f6777d
Laurent <laurent.rochette@codefresh.io>
2022-01-26 17:27:53 -0700

    moved from  wrongly named repo

---
+++ resources/CorpSite/image.sh
@@ -0,0 +1,2 @@
+docker build -t lrochette/argo-hub-workflows-servicenow-versions-1.0.0-integration:main /Users/lrochette/src/lrochette/argo-hub/workflows/servicenow/versions/1.0.0/images/integration
+docker push lrochette/argo-hub-workflows-servicenow-versions-1.0.0-integration:main
