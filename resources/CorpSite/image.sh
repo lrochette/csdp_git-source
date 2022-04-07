145221a8790c82727802baa6774982cd54ab093e
Laurent <laurent.rochette@codefresh.io>
2022-04-07 14:42:31 -0700

    fix ingress?
    Signed-off-by: Laurent <laurent.rochette@codefresh.io>

--- resources/CorpSite/image.sh
+++
@@ -1,2 +0,0 @@
-docker build -t lrochette/argo-hub-workflows-servicenow-versions-1.0.0-integration:main /Users/lrochette/src/lrochette/argo-hub/workflows/servicenow/versions/1.0.0/images/integration
-docker push lrochette/argo-hub-workflows-servicenow-versions-1.0.0-integration:main
